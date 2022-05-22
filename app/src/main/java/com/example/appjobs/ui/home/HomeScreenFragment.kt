69ea7171f8dab12b583ce799817dd87f81e656f3
brianA1998 <brian.alaniz@hotmail.com>
2022-05-22 20:44:18 -0300

    Agregando viewbinding y fragment de home

---
+++ app/src/main/java/com/example/appjobs/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,14 @@
+package com.example.appjobs.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.appjobs.R
+
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
