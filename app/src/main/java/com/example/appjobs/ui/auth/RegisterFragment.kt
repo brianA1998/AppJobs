bfeaff4cbd856467e22ad40f3bfd642f4cfbae07
brianA1998 <brian.alaniz@hotmail.com>
2022-06-12 17:08:43 -0300

    Funcion para validar los datos del usuario

--- app/src/main/java/com/example/appjobs/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/appjobs/ui/auth/RegisterFragment.kt
@@ -27,17 +27,55 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             val username = binding.editTextUsername.text.toString().trim()
             val password = binding.editTextPassword.text.toString().trim()
             val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
-            val email = binding.editTextEmail.toString().trim()
+            val email = binding.editTextEmailRegister.text.toString().trim()
 
-            
-            if (password != confirmPassword) {
-                binding.editTextConfirmPassword.error = "Password does not match"
-                binding.editTextPassword.error = "Password does not match"
-                return@setOnClickListener
-            }
 
-            Log.d("signUpData", "data : {$username , $password , $confirmPassword , $email}")
+            if (validateUserData(
+                    password,
+                    confirmPassword,
+                    email,
+                    username
+                )
+            ) return@setOnClickListener
+
+
+
+            Log.d("signUpData", "data : $username , $password , $confirmPassword , $email")
 
         }
+    }
+
+    private fun validateUserData(
+        password: String,
+        confirmPassword: String,
+        email: String,
+        username: String
+    ): Boolean {
+        if (password != confirmPassword) {
+            binding.editTextConfirmPassword.error = "Password does not match"
+            binding.editTextPassword.error = "Password does not match"
+            return true
+        }
+
+        if (password.isEmpty()) {
+            binding.editTextPassword.error = "password is empty"
+            return true
+        }
+
+        if (confirmPassword.isEmpty()) {
+            binding.editTextConfirmPassword.error = "confirm password is empty"
+            return true
+        }
+
+        if (email.isEmpty()) {
+            binding.editTextEmailRegister.error = "email is empty"
+            return true
+        }
+
+        if (username.isEmpty()) {
+            binding.editTextUsername.error = "username is empty"
+            return true
+        }
+        return false
     }
 }
