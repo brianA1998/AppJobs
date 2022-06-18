f4383e5658852646505a20e3f633ced61754e0a1
brianA1998 <brian.alaniz@hotmail.com>
2022-06-18 00:43:04 -0300

    registro de usuario funcional completamente

--- app/src/main/java/com/example/appjobs/data/remote/auth/AuthDataSource.kt
+++ app/src/main/java/com/example/appjobs/data/remote/auth/AuthDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.appjobs.data.remote.auth
 
+import com.example.appjobs.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 class AuthDataSource {
@@ -15,6 +17,11 @@ class AuthDataSource {
     suspend fun signUp(email: String, password: String, username: String): FirebaseUser? {
         val authResult =
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).await()
+        authResult.user?.uid?.let { uid ->
+            FirebaseFirestore.getInstance().collection("users").document(uid)
+                .set(User(email, username, "FOTO_URL.PNG")).await()
+        }
+
         return authResult.user
     }
 
