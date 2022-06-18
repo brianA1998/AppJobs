1ec5bf2d40758099d91ef415dcf3bc9d2ecc3971
brianA1998 <brian.alaniz@hotmail.com>
2022-06-18 03:20:34 -0300

    Creando el menu de navegacion y vinculando ese menu con el home!

---
+++ app/src/main/java/com/example/appjobs/ui/camera/CameraFragment.kt
@@ -0,0 +1,14 @@
+package com.example.appjobs.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appjobs.R
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+
+}
---
+++ app/src/main/java/com/example/appjobs/ui/profile/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.example.appjobs.ui.profile
+
+import androidx.fragment.app.Fragment
+import com.example.appjobs.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+
+}
