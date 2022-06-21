e9bbf16dd85d187bc63e82db534864d03e583a58
brianA1998 <brian.alaniz@hotmail.com>
2022-06-21 00:35:31 -0300

    Bottom navigation terminado

--- app/src/main/java/com/example/appjobs/MainActivity.kt
+++ app/src/main/java/com/example/appjobs/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.example.appjobs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.appjobs.core.hide
+import com.example.appjobs.core.show
 import com.example.appjobs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_ho st_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        observeDestinationChange(navController)
+
+    }
+
+    private fun observeDestinationChange(navController: NavController) {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+
+        }
     }
 }
