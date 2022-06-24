5cdf0000bcd3ba1948b27625f6a4f617c91021c7
brianA1998 <brian.alaniz@hotmail.com>
2022-06-24 00:50:31 -0300

    Comenzando a desarrollar el set up profile

--- app/src/main/java/com/example/appjobs/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/appjobs/presentation/auth/AuthViewModel.kt
@@ -27,6 +27,17 @@ class AuthViewModel(private val repo: AuthRepo) : ViewModel() {
             emit(Result.Failure(e))
         }
     }
+
+    fun updateUserProfile() = liveData(Dispatchers.IO){
+        emit(Result.Loading())
+        try {
+            emit(Result.Success(repo.signUp(email, password, username)))
+        } catch (e: Exception) {
+            emit(Result.Failure(e))
+        }
+    }
+
+
 }
 
 class AuthViewModelFactory(private val repo: AuthRepo) : ViewModelProvider.Factory {
---
+++ app/src/main/java/com/example/appjobs/ui/auth/SetupProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.example.appjobs.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appjobs.R
+import com.example.appjobs.databinding.FragmentProfileBinding
+
+
+class SetupProfileFragment : Fragment(R.layout.fragment_setup_profile) {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
+    }
+
+}
